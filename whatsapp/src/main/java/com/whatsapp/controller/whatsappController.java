530134f3006bc287c8ea29be7ea5cc180128091e
DESKTOP-T8L64CQ\SASI <sasidhar@binacraftsolutions.com>
2024-07-30 17:13:43 +0530

    github

---
+++ whatsapp/src/main/java/com/whatsapp/controller/whatsappController.java
@@ -0,0 +1,47 @@
+package com.whatsapp.controller;
+
+public class whatsappController implements whatsappControllerInterface {
+
+	@Override
+	public void createProfileController() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void viewProfileController() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteProfileController() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void editProfileController() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void searchprofileController() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void viewallProfileController() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void loginProfileController() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ whatsapp/src/main/java/com/whatsapp/controller/whatsappControllerInterface.java
@@ -0,0 +1,19 @@
+package com.whatsapp.controller;
+
+public interface whatsappControllerInterface {
+
+	void createProfileController();
+
+	void viewProfileController();
+
+	void deleteProfileController();
+
+	void editProfileController();
+
+	void searchprofileController();
+
+	void viewallProfileController();
+
+	void loginProfileController();
+
+}
